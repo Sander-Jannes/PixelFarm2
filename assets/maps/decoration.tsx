041471ff85ec50d52cd6ab4878d04cc21de74284
Sander-Jannes <sanderjannes1@gmail.com>
2025-08-04 10:22:54 +0200

    map update

---
+++ assets/maps/decoration.tsx
@@ -0,0 +1,179 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="decoration" tilewidth="128" tileheight="128" tilecount="40" columns="8">
+ <image source="../images/tile_sets/blocks/decoration.png" width="1028" height="640"/>
+ <tile id="0">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="group" value="decoration"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ assets/maps/trees2.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="trees2" tilewidth="128" tileheight="256" tilecount="20" columns="10">
+ <image source="../images/tile_sets/blocks/trees2.png" width="1280" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+  <animation>
+   <frame tileid="10" duration="500"/>
+   <frame tileid="11" duration="500"/>
+   <frame tileid="12" duration="500"/>
+   <frame tileid="13" duration="500"/>
+   <frame tileid="14" duration="500"/>
+   <frame tileid="15" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="group" value="trees"/>
+  </properties>
+ </tile>
+</tileset>
