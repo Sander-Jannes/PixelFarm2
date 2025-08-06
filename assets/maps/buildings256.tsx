f79753051ed70f8f1a23eb002f8b5398574d2b76
Sander-Jannes <sanderjannes1@gmail.com>
2025-08-06 19:53:28 +0200

    selecting fields

---
+++ assets/maps/buildings256.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="buildings256" tilewidth="256" tileheight="256" tilecount="4" columns="2">
+ <image source="../images/tile_sets/blocks/buildings256.png" width="512" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="group" value="buildings"/>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="group" value="buildings"/>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="group" value="buildings"/>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="group" value="buildings"/>
+   <property name="name" value=""/>
+  </properties>
+ </tile>
+</tileset>
